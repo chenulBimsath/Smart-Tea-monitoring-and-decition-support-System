d17ff91e79b85b9d9f2aa50afbcc0d671832305d
oshanWijesinghe <w2052044@westminster.ac.uk>
2026-02-06 13:15:28 +0530

    spring init

---
+++ backend/tea_backend/src/main/java/com/tea_management/tea_backend/TeaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tea_management.tea_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/tea_backend/src/test/java/com/tea_management/tea_backend/TeaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tea_management.tea_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
