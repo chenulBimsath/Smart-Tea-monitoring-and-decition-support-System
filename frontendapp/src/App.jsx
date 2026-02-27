7f26c86a07a31223a04a28b54aa74d227ad5bd60
Dulinanethmal <dulinanethmal1@gmail.com>
2026-02-27 13:33:19 +0530

    add routing for landing page and dashboard
    Added React Router and navigation between landing page and SDGP dashboard.

--- frontendapp/src/App.jsx
+++ frontendapp/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+// landing page components
+import LandingPage from "./LandingPage";
+=======
+>>>>>>> Stashed changes
 import Navbar from "./components/landing_page/navbar";
 import Splash from "./components/landing_page/splash";
 import Features from "./components/landing_page/features";
 import HowItWorks from "./components/landing_page/howItWorks";
 import Contact from "./components/landing_page/contact";
 import SignInPopup from "./components/landing_page/signinPopup";
+<<<<<<< Updated upstream
+=======
+>>>>>>> 5abdadbee4cdcba12f3bc4334b614e2e385e93d0
+>>>>>>> Stashed changes
 
+// sdgp dashboard
+import SDGPApp from "./SDGPApp";
 
 export default function App() {
-  const [showSignIn, setShowSignIn] = useState(false);
-
-
   return (
-    <>
-      
-      <Navbar onGetStarted={() => setShowSignIn(true)} />
-
-      <Splash />
-      <Features />
-      <HowItWorks />
-      <Contact />
-
-
-      {showSignIn && (
-  <SignInPopup
-    closePopup={() => setShowSignIn(false)}
-  />
-)}
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<SDGPApp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
