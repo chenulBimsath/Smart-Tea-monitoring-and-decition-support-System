c050a161741d5e8a0f28fa5c5da370a2c9fd6fdf
nipunGajasingha <nipun.akarshana.k@gmail.com>
2026-03-06 19:58:58 +0530

    Update routing
    Removed duplicates

--- frontendapp/src/SDGPApp.jsx
+++ frontendapp/src/SDGPApp.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
 
-import DashboardCards from "./components/sdgp/DashboardCards";
 import FieldMap from "./components/sdgp/FieldMap";
 import FieldData from "./components/sdgp/FieldData";
 import ClimateRisk from "./components/sdgp/ClimateRisk";
@@ -14,26 +13,29 @@ import NewDivisionData from "./components/sdgp/NewDivisionData";
 import AddFieldData from "./components/sdgp/AddFieldData";
 
 export default function SDGPApp() {
-  const [page, setPage] = useState("dashboard");
+
+  const [page, setPage] = useState("fieldmap");
 
   return (
     <>
       <Header />
 
       <div className="app-layout">
+
         <Sidebar page={page} setPage={setPage} />
 
         <main className="app-main">
-          {page === "dashboard" && <DashboardCards />}
+
           {page === "fieldmap" && <FieldMap />}
           {page === "fielddata" && <FieldData setPage={setPage} />}
-          {page === "climate" && <ClimateRisk />}
-          {page === "rangala" && <RangalaData setPage={setPage} />}
-          {page === "rangaladivisions" && <RangalaDivisions setPage={setPage} />}
-          {page === "rangaladata" && <RangalaData setPage={setPage} />}
-          {page === "newdivisiondata" && <NewDivisionData setPage={setPage} />}
-          {page === "addfield" && <AddFieldData setPage={setPage} />}
+          {page === "climaterisk" && <ClimateRisk />}
+          {page === "rangaladivisions" && (<RangalaDivisions setPage={setPage} />)}
+          {page === "rangaladata" && (<RangalaData setPage={setPage} />)}
+          {page === "newdivisiondata" && (<NewDivisionData setPage={setPage} />)}
+          {page === "addfield" && (<AddFieldData setPage={setPage} />)}
+
         </main>
+
       </div>
     </>
   );
