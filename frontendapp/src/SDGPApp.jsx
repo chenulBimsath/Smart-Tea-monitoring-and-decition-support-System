4efab201d348286aeb1fc0cd30f60ad9c9e45e85
Chenul Bimsath Mallawathanthri <chenul.20231842@iit.ac.lk>
2026-03-09 17:29:52 +0530

    Update SDGPApp.jsx
    Updated the root (Fixed the issue of duplicate pages)

--- frontendapp/src/SDGPApp.jsx
+++ frontendapp/src/SDGPApp.jsx
@@ -2,51 +2,46 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
+
 import FieldMap from "./components/sdgp/FieldMap";
 import FieldData from "./components/sdgp/FieldData";
 import ClimateRisk from "./components/sdgp/ClimateRisk";
 import MyProfile from "./components/sdgp/MyProfile";
+
 import RangalaData from "./components/sdgp/RangalaData";
 import RangalaDivisions from "./components/sdgp/RangalaDivisions";
 import NewDivisionData from "./components/sdgp/NewDivisionData";
 import AddFieldData from "./components/sdgp/AddFieldData";
+
 import FertilizerData from "./components/sdgp/FertilizerData";
 import FertilizerRangalaDivisions from "./components/sdgp/FertilizerRangalaDivisions";
 import RangalaFertilizerData from "./components/sdgp/RangalaFertilizerData";
 import NewDivisionFertilizerData from "./components/sdgp/NewDivisionFertilizerData";
-import CropYields from "./components/sdgp/CropYields";
 import AddFertilizerData from "./components/sdgp/AddFertilizerData";
 
+import CropYields from "./components/sdgp/CropYields";
 
 export default function SDGPApp() {
-
   const [page, setPage] = useState("fieldmap");
 
   return (
     <>
       <Header />
 
-      <div className="app-layout"> <Sidebar page={page} setPage={setPage} />
+      <div className="app-layout">
+        <Sidebar page={page} setPage={setPage} />
 
         <main className="app-main">
 
           {page === "fieldmap" && <FieldMap />}
 
-
-          {page === "fielddata" && <FieldData setPage={setPage} />}
-
-
-          {page === "fielddata" && (<FieldData setPage={setPage} />)}
+          {page === "fielddata" && (
+            <FieldData setPage={setPage} />
+          )}
 
           {page === "climaterisk" && <ClimateRisk />}
-          {page === "profile" && <MyProfile />}
-          {page === "rangaladivisions" && ( <RangalaDivisions setPage={setPage} /> )}
-          {page === "rangaladata" && ( <RangalaData setPage={setPage} />)}
-          {page === "newdivisiondata" && (<NewDivisionData setPage={setPage} />         )}
-          {page === "addfield" && (<AddFieldData setPage={setPage} /> )}
-          {page === "crop_yields" && (<CropYields setPage={setPage} />)}
-          {page === "profile" && <MyProfile />}
 
+          {page === "profile" && <MyProfile />}
 
           {page === "rangaladivisions" && (
             <RangalaDivisions setPage={setPage} />
@@ -64,31 +59,31 @@ export default function SDGPApp() {
             <AddFieldData setPage={setPage} />
           )}
 
-          {page === "rangaladivisions" && ( <RangalaDivisions setPage={setPage} /> )}
+          {page === "crop_yields" && (
+            <CropYields setPage={setPage} />
+          )}
 
-          {page === "fielddata" && (<FieldData setPage={setPage} />)}
-          {page === "climaterisk" && <ClimateRisk />}
-          {page === "profile" && <MyProfile />}
-          {page === "rangaladivisions" && ( <RangalaDivisions setPage={setPage} /> )}
+          {page === "fertilizerdata" && (
+            <FertilizerData setPage={setPage} />
+          )}
 
-          {page === "rangaladata" && ( <RangalaData setPage={setPage} />  )}
-          {page === "newdivisiondata" && (  <NewDivisionData setPage={setPage} /> )}
-          {page === "addfield" && ( <AddFieldData setPage={setPage} />)}
-          {page === "crop_yields" && (<CropYields setPage={setPage} />)}
-          {page === "fertilizerdata" && (<FertilizerData setPage={setPage} />)}
-          {page === "fertilizerrangaladivisions" && (<FertilizerRangalaDivisions setPage={setPage} />)}
-          {page === "rangalafertilizer" && (<RangalaFertilizerData setPage={setPage} />)}
-          {page === "newdivisionfertilizer" && (<NewDivisionFertilizerData setPage={setPage} />)}
-          {page === "addfertilizer" && (<AddFertilizerData setPage={setPage} />)}
-          {page === "rangalafertilizer" && (<RangalaFertilizerData setPage={setPage} />)}
-          {page === "newdivisionfertilizer" && (<NewDivisionFertilizerData setPage={setPage} />)}
-          {page === "addfertilizer" && (<AddFertilizerData setPage={setPage} />)}
+          {page === "fertilizerrangaladivisions" && (
+            <FertilizerRangalaDivisions setPage={setPage} />
+          )}
 
+          {page === "rangalafertilizer" && (
+            <RangalaFertilizerData setPage={setPage} />
+          )}
 
+          {page === "newdivisionfertilizer" && (
+            <NewDivisionFertilizerData setPage={setPage} />
+          )}
 
+          {page === "addfertilizer" && (
+            <AddFertilizerData setPage={setPage} />
+          )}
 
         </main>
-
       </div>
     </>
   );
