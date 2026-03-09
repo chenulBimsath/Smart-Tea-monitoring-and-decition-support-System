ec311015f0d5b37428ab8d9b0c6c4a1a3f2e205e
Dulinanethmal <dulinanethmal1@gmail.com>
2026-03-09 16:32:44 +0530

    Update routing

--- frontendapp/src/SDGPApp.jsx
+++ frontendapp/src/SDGPApp.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
-
 import FieldMap from "./components/sdgp/FieldMap";
 import FieldData from "./components/sdgp/FieldData";
 import ClimateRisk from "./components/sdgp/ClimateRisk";
@@ -12,6 +10,15 @@ import RangalaData from "./components/sdgp/RangalaData";
 import RangalaDivisions from "./components/sdgp/RangalaDivisions";
 import NewDivisionData from "./components/sdgp/NewDivisionData";
 import AddFieldData from "./components/sdgp/AddFieldData";
+<<<<<<< Updated upstream
+=======
+import FertilizerData from "./components/sdgp/FertilizerData";
+import FertilizerRangalaDivisions from "./components/sdgp/FertilizerRangalaDivisions";
+import RangalaFertilizerData from "./components/sdgp/RangalaFertilizerData";
+import NewDivisionFertilizerData from "./components/sdgp/NewDivisionFertilizerData";
+import CropYields from "./components/sdgp/CropYields";
+import AddFertilizerData from "./components/sdgp/AddFertilizerData";
+>>>>>>> Stashed changes
 
 export default function SDGPApp() {
 
@@ -21,19 +28,22 @@ export default function SDGPApp() {
     <>
       <Header />
 
-      <div className="app-layout">
-
-        <Sidebar page={page} setPage={setPage} />
+      <div className="app-layout"> <Sidebar page={page} setPage={setPage} />
 
         <main className="app-main">
 
           {page === "fieldmap" && <FieldMap />}
+<<<<<<< Updated upstream
 
           {page === "fielddata" && <FieldData setPage={setPage} />}
 
+=======
+          {page === "fielddata" && (<FieldData setPage={setPage} />)}
+>>>>>>> Stashed changes
           {page === "climaterisk" && <ClimateRisk />}
 
           {page === "profile" && <MyProfile />}
+<<<<<<< Updated upstream
 
           {page === "rangaladivisions" && (
             <RangalaDivisions setPage={setPage} />
@@ -50,6 +60,21 @@ export default function SDGPApp() {
           {page === "addfield" && (
             <AddFieldData setPage={setPage} />
           )}
+=======
+          {page === "rangaladivisions" && ( <RangalaDivisions setPage={setPage} /> )}
+          {page === "rangaladata" && ( <RangalaData setPage={setPage} />  )}
+          {page === "newdivisiondata" && (  <NewDivisionData setPage={setPage} /> )}
+          {page === "addfield" && ( <AddFieldData setPage={setPage} />)}
+          {page === "crop_yields" && (<CropYields setPage={setPage} />)}
+          {page === "fertilizerdata" && (<FertilizerData setPage={setPage} />)}
+          {page === "fertilizerrangaladivisions" && (<FertilizerRangalaDivisions setPage={setPage} />)}
+          {page === "rangalafertilizer" && (<RangalaFertilizerData setPage={setPage} />)}
+          {page === "newdivisionfertilizer" && (<NewDivisionFertilizerData setPage={setPage} />)}
+          {page === "addfertilizer" && (<AddFertilizerData setPage={setPage} />)}
+          {page === "rangalafertilizer" && (<RangalaFertilizerData setPage={setPage} />)}
+          {page === "newdivisionfertilizer" && (<NewDivisionFertilizerData setPage={setPage} />)}
+          {page === "addfertilizer" && (<AddFertilizerData setPage={setPage} />)}
+>>>>>>> Stashed changes
 
         </main>
 
