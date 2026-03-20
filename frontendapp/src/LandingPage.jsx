bfccec45b0c13b11383e693e71aa64d16d7e0de0
Chenul Bimsath Mallawathanthri <chenul.20231842@iit.ac.lk>
2026-03-20 23:39:01 +0530

    Fixed the Landing Page Issue
    fixed the landing page issue that occurred the conflict

--- frontendapp/src/LandingPage.jsx
+++ frontendapp/src/LandingPage.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-// 👇 Notice how the ends of these paths are now lowercase/camelCase
+
 import Navbar from "./components/layout/navbar";
 import Splash from "./components/pages/splash";
 import Features from "./components/pages/features";
 import HowItWorks from "./components/pages/howItWorks";
 import Contact from "./components/pages/contact";
 import SignInPopup from "./components/pages/signinPopup";
-import <SignUpPopup> from "./components/pages/signUpPopup";
+import <SignUpPopup> from 
 
 export default function LandingPage() {
   const [showSignIn, setShowSignIn] = useState(false);
