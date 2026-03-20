88ac4c2b7667225008764e1a5f051b03a8d53650
Chenul Bimsath Mallawathanthri <chenul.20231842@iit.ac.lk>
2026-03-20 23:08:47 +0530

    Update route
    updated route to signup popup page

--- frontendapp/src/LandingPage.jsx
+++ frontendapp/src/LandingPage.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
-import Navbar from "./components/layout/Navbar";
-import Splash from "./components/pages/Splash";
-import Features from "./components/pages/Features";
+import Navbar    from "./components/layout/Navbar";
+import Splash    from "./components/pages/Splash";
+import Features  from "./components/pages/Features";
 import HowItWorks from "./components/pages/HowItWorks";
-import Contact from "./components/pages/Contact";
-import SignInPopup from "./components/pages/SigninPopup";
+import Contact   from "./components/pages/Contact";
+import SignInPopup  from "./components/pages/SigninPopup";
+import SignUpPopup  from "./components/pages/SignUpPopup";
 
 export default function LandingPage() {
   const [showSignIn, setShowSignIn] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
 
   return (
     <>
@@ -20,7 +22,17 @@ export default function LandingPage() {
       <Contact />
 
       {showSignIn && (
-        <SignInPopup closePopup={() => setShowSignIn(false)} />
+        <SignInPopup
+          closePopup={() => setShowSignIn(false)}
+          openSignUp={() => { setShowSignIn(false); setShowSignUp(true); }}
+        />
+      )}
+
+      {showSignUp && (
+        <SignUpPopup
+          closePopup={() => setShowSignUp(false)}
+          openSignIn={() => { setShowSignUp(false); setShowSignIn(true); }}
+        />
       )}
     </>
   );
